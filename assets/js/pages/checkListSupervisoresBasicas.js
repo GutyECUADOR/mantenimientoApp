35023d771080707fe8f66995a61f9fd1018bb496
José Gutiérrez <gutiecuador@gmail.com>
2019-04-01 13:20:18 -0500

    Avance form checklist
    Se agrega y hacen pruebas de vue y axios para el incio de la integracion con el API

--- assets/js/pages/checkListSupervisoresBasicas.js
+++ assets/js/pages/checkListSupervisoresBasicas.js
@@ -1,9 +1,55 @@
+var app = new Vue({
+    el: '#formActividadesBasicas',
+    data () {
+        return {
+            txtCIRUC: '',
+            usuarioIdentificado: '',
+            info: null
+        }
+    },
+    methods: {
+        greet: function (event) {
+          // `this` inside methods point to the Vue instance
+          console.log(this.txtCIRUC);
+          
+        },
+        searchCIRUC () {
+            altair_helpers.content_preloader_show('regular'); /* Loading SVG */
+            axios
+                .get(`http://localhost/PHPProjects/mantenimientoApp/views/modulos/ajax/API_estadisticas.php?action=test`)
+                .then(response => {
+                    this.usuarioIdentificado = response.data.data;
+                    console.log(response);
+                    console.log(this.usuarioIdentificado);
+                    altair_helpers.content_preloader_hide(); /* Stop Loading SVG */
+                })
+                .catch(error => console.log(error))
+        }
+    },
+    filters: {
+        capitalize: function (value) {
+          if (!value) return '(NO IDENTIFICADO)'
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1)
+        }
+      }
+    
+  })
+
+
 $(function() {
-    // tasks list
-    altair_todo.init();
+
+    
+    app.init();
+    var $save_form_submit = $('#save_form_submit');
+
+    $save_form_submit.on('click', function(e) {
+        e.preventDefault();
+        app.save_form();
+    });
 });
 
-altair_todo = {
+app = {
     init: function() {
         var $todo_list = $('#todo_list');
         $todo_list.find('input:checkbox')
@@ -13,6 +59,14 @@ altair_todo = {
             .on('ifUnchecked', function(){
                 $(this).closest('li').removeClass('md-list-item-disabled');
             });
+    },
+    save_form: function() {
+        let $formMain = $('#formActividadesBasicas'); //ID formulario 
+
+        let form_serialized = JSON.stringify($formMain.serializeObject(), null, 2);
+        UIkit.modal.alert('<p>Confirme datos del formulario:</p><pre>' + form_serialized + '</pre>');
+        
+      
     }
 };
 
--- views/modulos/checkActBasicasSup.php
+++ views/modulos/checkActBasicasSup.php
@@ -6,8 +6,11 @@
       <?php include 'sis_modules/sidebar_main.php'?>
     <!-- main sidebar end -->
 
+    
+
     <!-- CONTENIDO DE LA SECCION -->    
     <div id="page_content">
+        
         <div id="page_content_inner">
 
             <div class="uk-width-medium-8-10 uk-container-center">
@@ -19,138 +22,142 @@
                         <h3 class="md-card-toolbar-heading-text large">
                             Actividades basicas a cumplir
                         </h3>
+
                     </div>
-                    <div class="md-card-content" id="todo_list" style="padding-bottom: 50px;">
 
-                        <h2 class="heading_list">Informacion Principal: </h2>
+                    
 
-                        <div class="uk-grid" data-uk-grid-margin> 
-                            <div class="uk-input-group uk-width-medium-1-3">
-                                <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-user"></i></span>
-                                <label>Cedula del supervisor: </label>
-                                <input type="number" class="md-input label-fixed" placeholder="170000000000" />
-                            </div>
+                    <form action="" id="formActividadesBasicas" >
+                        <div class="md-card-content" style="padding-bottom: 50px;">
 
-                            <div class="uk-input-group uk-width-medium-1-3">
-                                <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-cog"></i></span>
-                                <label for="product_edit_tecnico" class="uk-form-label">Empresa: </label>
-                                <select id="select_demo_2" class="md-input" data-uk-tooltip="{pos:'top'}" title="Seleccione empresa">
-                                        <option value="" disabled selected hidden>Seleccione por favor</option>
-                                        <option value="a">MODELO</option>
-                                </select>
-                            </div>
+                            <h2 class="heading_list">Informacion Solicitante: {{ usuarioIdentificado | capitalize }}</h2>
 
-                            <div class="uk-input-group uk-width-medium-1-3">
-                                <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-cog"></i></span>
-                                <label for="product_edit_tecnico" class="uk-form-label">Bodega: </label>
-                                <select id="select_demo_2" class="md-input" data-uk-tooltip="{pos:'top'}" title="Seleccione bodega">
-                                        <option value="" disabled selected hidden>Seleccione por favor</option>
-                                        <option value="a">PRUEBA</option>
-                                </select>
-                            </div>
+                            <div class="uk-grid" data-uk-grid-margin> 
+                                <div class="uk-input-group uk-width-medium-1-3">
+                                    <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-user"></i></span>
+                                    <label>Cedula del supervisor: </label>
+                                    <input type="number" v-model="txtCIRUC" v-on:keyup="searchCIRUC" name="txtCIRUC" class="md-input label-fixed" placeholder="170000000000" />
+                                </div>
 
-                            
+                                <div class="uk-input-group uk-width-medium-1-3">
+                                    <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-cog"></i></span>
+                                    <label class="uk-form-label">Empresa: </label>
+                                    <select id="selectEmpresa" name="selectEmpresa" class="md-input" data-uk-tooltip="{pos:'top'}" title="Seleccione empresa">
+                                            <option value="" disabled selected hidden>Seleccione por favor</option>
+                                            <option value="a">MODELO</option>
+                                    </select>
+                                </div>
 
-                        </div>
+                                <div class="uk-input-group uk-width-medium-1-3">
+                                    <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-cog"></i></span>
+                                    <label class="uk-form-label">Bodega: </label>
+                                    <select id="selectBodega" name="selectBodega" class="md-input" data-uk-tooltip="{pos:'top'}" title="Seleccione bodega">
+                                            <option value="" disabled selected hidden>Seleccione por favor</option>
+                                            <option value="a">PRUEBA</option>
+                                    </select>
+                                </div>
 
-                        <h2 class="heading_list">Semana, Hora de Ingreso/Salida: </h2>
-                        <div class="uk-grid" data-uk-grid-margin>
-
-                            <div class="uk-input-group uk-width-medium-1-3">
-                                <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-calendar"></i></span>
-                                <label for="product_edit_tecnico" class="uk-form-label">Semana: </label>
-                                <select id="select_demo_2" class="md-input" data-uk-tooltip="{pos:'top'}" title="Seleccione semana">
-                                        <option value="" disabled selected hidden>Seleccione por favor</option>
-                                        <option value="a">SEMANA 1</option>
-                                        <option value="b">SEMANA 2</option>
-                                        <option value="c">SEMANA 3</option>
-                                        <option value="c">SEMANA 4</option>
-                                </select>
-                            </div>
-
-                            <div class="uk-input-group uk-width-medium-1-3">
-                                <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-clock-o"></i></span>
-                                <label for="uk_tp_1">Hora de Inicio</label>
-                                <input class="md-input label-fixed" type="time" id="uk_tp_1" data-uk-timepicker>
-                                
-                            </div>
-                            
-                            <div class="uk-input-group uk-width-medium-1-3">
-                                <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-clock-o"></i></span>
-                                <label for="uk_tp_1">Hora de finalizacion</label>
-                                <input class="md-input label-fixed" type="time" id="uk_tp_1" data-uk-timepicker>
-                               
-                            </div>
-                        </div>
-
-                        <h2 class="heading_list">Detalle: </h2>
-                        <div>
-
-                            <ul class="md-list md-list-addon uk-margin-small-bottom uk-nestable" data-uk-nestable="{ maxDepth:2,handleClass:'md-list-content'}">
                                 
 
-                                <li>
-                                    <div class="md-list-addon-element">
-                                        <input type="checkbox" data-md-icheck />
-                                    </div>
-                                    <div class="md-list-content">
-                                        <span class="md-list-heading"> Revision de CheckList </span>
-                                        <span class="uk-text-small uk-text-muted">Se ha revisado los checklist de los locales.</span>
-                                    </div>
-                                    <div class="md-input-wrapper md-input-filled">
-                                        <input type="text" class="md-input" placeholder="Comentario del item">
-                                        <span class="md-input-bar"></span>
-                                    </div>
-                                </li>
+                            </div>
 
-                                <li>
-                                    <div class="md-list-addon-element">
-                                        <input type="checkbox" data-md-icheck />
-                                    </div>
-                                    <div class="md-list-content">
-                                        <span class="md-list-heading"> Revision de Couching de ventas</span>
-                                        <span class="uk-text-small uk-text-muted">Se ha dado revision al trabajo del personal de ventas.</span>
-                                    </div>
-                                    <div class="md-input-wrapper md-input-filled">
-                                        <input type="text" class="md-input" placeholder="Comentario del item">
-                                        <span class="md-input-bar"></span>
-                                    </div>
-                                </li>
+                            <h2 class="heading_list">Semana, Hora de Ingreso/Salida: </h2>
+                            <div class="uk-grid" data-uk-grid-margin>
 
-                                <li>
-                                    <div class="md-list-addon-element">
-                                        <input type="checkbox" data-md-icheck />
-                                    </div>
-                                    <div class="md-list-content">
-                                        <span class="md-list-heading"> Revision de Couching de ventas</span>
-                                        <span class="uk-text-small uk-text-muted">Se ha dado revision al trabajo del personal de ventas.</span>
-                                    </div>
-                                    <div class="md-input-wrapper md-input-filled">
-                                        <input type="text" class="md-input" placeholder="Comentario del item">
-                                        <span class="md-input-bar"></span>
-                                    </div>
-                                </li>
+                                <div class="uk-input-group uk-width-medium-1-3">
+                                    <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-calendar"></i></span>
+                                    <label class="uk-form-label">Semana: </label>
+                                    <select id="selectSemana" name="selectSemana" class="md-input" data-uk-tooltip="{pos:'top'}" title="Seleccione semana">
+                                            <option value="" disabled selected hidden>Seleccione por favor</option>
+                                            <option value="a">SEMANA 1</option>
+                                            <option value="b">SEMANA 2</option>
+                                            <option value="c">SEMANA 3</option>
+                                            <option value="c">SEMANA 4</option>
+                                    </select>
+                                </div>
 
-                                <li>
-                                    <div class="md-list-addon-element">
-                                        <input type="checkbox" data-md-icheck />
-                                    </div>
-                                    <div class="md-list-content">
-                                        <span class="md-list-heading"> Revision de Couching de ventas</span>
-                                        <span class="uk-text-small uk-text-muted">Se ha dado revision al trabajo del personal de ventas.</span>
-                                    </div>
-                                    <div class="md-input-wrapper md-input-filled">
-                                        <input type="text" class="md-input" placeholder="Comentario del item">
-                                        <span class="md-input-bar"></span>
-                                    </div>
-                                </li>
-                            </ul>
-                            
+                                <div class="uk-input-group uk-width-medium-1-3">
+                                    <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-clock-o"></i></span>
+                                    <label> Hora de Inicio</label>
+                                    <input id="txt_horaINI" name="txt_horaINI" class="md-input label-fixed" type="time" data-uk-timepicker>
+                                    
+                                </div>
+                                
+                                <div class="uk-input-group uk-width-medium-1-3">
+                                    <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-clock-o"></i></span>
+                                    <label>Hora de finalizacion</label>
+                                    <input id="txt_horaFIN" name="txt_horaFIN" class="md-input label-fixed" type="time" data-uk-timepicker>
+                                
+                                </div>
+                            </div>
+
+                            <h2 class="heading_list">Detalle: </h2>
+                            <div>
+                                <!-- Checklists -->
+                                <ul class="md-list md-list-addon uk-margin-small-bottom uk-nestable" data-uk-nestable="{ maxDepth:2,handleClass:'md-list-content'}">
+                                    
+                                    <li>
+                                        <div class="md-list-addon-element">
+                                            <input type="checkbox" name="chklist" data-md-icheck />
+                                        </div>
+                                        <div class="md-list-content">
+                                            <span class="md-list-heading"> Revision de CheckList </span>
+                                            <span class="uk-text-small uk-text-muted">Se ha revisado los checklist de los locales.</span>
+                                        </div>
+                                        <div class="md-input-wrapper md-input-filled">
+                                            <input type="text" class="md-input" placeholder="Comentario del item">
+                                            <span class="md-input-bar"></span>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="md-list-addon-element">
+                                            <input type="checkbox" name="chklist" data-md-icheck />
+                                        </div>
+                                        <div class="md-list-content">
+                                            <span class="md-list-heading"> Revision de Couching de ventas</span>
+                                            <span class="uk-text-small uk-text-muted">Se ha dado revision al trabajo del personal de ventas.</span>
+                                        </div>
+                                        <div class="md-input-wrapper md-input-filled">
+                                            <input type="text" class="md-input" placeholder="Comentario del item">
+                                            <span class="md-input-bar"></span>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="md-list-addon-element">
+                                            <input type="checkbox" data-md-icheck />
+                                        </div>
+                                        <div class="md-list-content">
+                                            <span class="md-list-heading"> Revision de Couching de ventas</span>
+                                            <span class="uk-text-small uk-text-muted">Se ha dado revision al trabajo del personal de ventas.</span>
+                                        </div>
+                                        <div class="md-input-wrapper md-input-filled">
+                                            <input type="text" class="md-input" placeholder="Comentario del item">
+                                            <span class="md-input-bar"></span>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="md-list-addon-element">
+                                            <input type="checkbox" data-md-icheck />
+                                        </div>
+                                        <div class="md-list-content">
+                                            <span class="md-list-heading"> Revision de Couching de ventas</span>
+                                            <span class="uk-text-small uk-text-muted">Se ha dado revision al trabajo del personal de ventas.</span>
+                                        </div>
+                                        <div class="md-input-wrapper md-input-filled">
+                                            <input type="text" class="md-input" placeholder="Comentario del item">
+                                            <span class="md-input-bar"></span>
+                                        </div>
+                                    </li>
+                                </ul>
+                                
+                                </div>
+
                         </div>
-
-
-                        
-                    </div>
+                    </form>
+                    
                 </div>
             </div>
 
@@ -159,7 +166,7 @@
 
      <!-- FAB Button -->  
     <div class="md-fab-wrapper">
-        <a class="md-fab md-fab-accent" href="#new_todo">
+        <a class="md-fab md-fab-accent" id="save_form_submit">
             <i class="material-icons">save</i>
         </a>
     </div>
